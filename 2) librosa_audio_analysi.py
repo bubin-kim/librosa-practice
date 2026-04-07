94bd4d2386893c5fcd54ac8f58b4d0616c1f6ed6
bubin-kim <bbin304@naver.com>
2026-04-07 11:16:46 +0900

    2) librosa_audio_analysi.py

--- 2) librosa_audio_analysi.py
+++ 2) librosa_audio_analysi.py
@@ -12,11 +12,11 @@ print(f"데이터 길이(d): {len(d)}개")
 plt.figure(figsize=(10, 8))
 
 plt.subplot(2, 1, 1)
-librosa.display.waveshow(d, sr=sr)
+librosa.display.waveshow(d, sr=sr)  # # x축: 시간, y축: 진폭
 plt.title('Visualizing Sound Variations')
 
-S = librosa.feature.melspectrogram(y=d, sr=sr)
-S_dB = librosa.power_to_db(S, ref=np.max)  # 데시벨 단위로 변환
+S = librosa.feature.melspectrogram(y=d, sr=sr)  # 사람의 청각 특성을 반영한 Mel scale 사용
+S_dB = librosa.power_to_db(S, ref=np.max)  # 데시벨 단위로 변환 -> 시각적으로 더 명확하게 보이게 하기 위함
 
 plt.subplot(2, 1, 2)
 librosa.display.specshow(S_dB, sr=sr, x_axis='time', y_axis='mel')
